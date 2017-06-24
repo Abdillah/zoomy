afef2b20c95f038487249cb3d10bfb2634511534
Hernawan Fa'iz Abdillah <hernawan.faiz@abdillah.me>
2017-06-24 16:31:15 +0700

    Change program to use the new DRM buffer abstraction

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate drm;
 extern crate mmap;
 
 mod mode;
+mod buffer;
 
 use std::fs::OpenOptions;
 use self::drm::drm_mode::Connection;
@@ -73,82 +74,21 @@ fn main() {
             conn: &connectors[i],
             mode: &modes[i],
             crtc: &crtcs[i].as_ref().unwrap(),
+
             height: height,
             width: width,
-            size: 0,
-            stride: 0,
-            handle: 0,
-            fb: 0,
         })
     }
 
-    // Creating dumb framebuffer
-    let mut ffi_create_dumb_struct = ffi::drm_mode_create_dumb::default();
-    ffi_create_dumb_struct.width = available_modes[0].width as u32;
-    ffi_create_dumb_struct.height = available_modes[0].height as u32;
-    ffi_create_dumb_struct.bpp = 32;
-    ffi_create_dumb_struct = unsafe {
-        let ret = libc::ioctl(
-            file.as_raw_fd(),
-            DRM_IOCTL_MODE_CREATE_DUMB,
-            (&ffi_create_dumb_struct as *const _) as *const libc::c_void
-        );
-        if ret < 0 {
-            None
-        } else {
-            Some(ffi_create_dumb_struct)
-        }
-    }.expect("Failed to CREATE_DUMB");
-    available_modes[0].stride = ffi_create_dumb_struct.pitch;
-    available_modes[0].size   = ffi_create_dumb_struct.size;
-    available_modes[0].handle = ffi_create_dumb_struct.handle;
-
-    // Add framebuffer
-    let buffer_id = drm::drm_mode::add_fb(
-        file.as_raw_fd(),
-        available_modes[0].width as u32,
-        available_modes[0].height as u32,
-        24,
-        32,
-        available_modes[0].stride,
-        available_modes[0].handle,
-    ).expect("Failed to ADD_FB");
-
-    // Map dumb framebuffer
-    use drm::ffi::drm_mode as ffi;
-    let mut ffi_map_dumb_struct = ffi::drm_mode_map_dumb::default();
-    ffi_map_dumb_struct.handle = available_modes[0].handle;
-    ffi_map_dumb_struct = unsafe {
-        let ret = libc::ioctl(
-            file.as_raw_fd(),
-            DRM_IOCTL_MODE_MAP_DUMB,
-            (&ffi_map_dumb_struct as *const _) as *const libc::c_void
-        );
-        if ret < 0 {
-            None
-        } else {
-            Some(ffi_map_dumb_struct)
-        }
-    }.expect("Failed to MAP_DUMB");
-
-    // MMAP
-    let moption = [
-        mmap::MapOption::MapReadable,
-        mmap::MapOption::MapWritable,
-        mmap::MapOption::MapFd(file.as_raw_fd()),
-        mmap::MapOption::MapOffset(ffi_map_dumb_struct.offset as usize),
-        mmap::MapOption::MapNonStandardFlags(0x01) // MAP_SHARED
-    ];
-    let map = mmap::MemoryMap::new(available_modes[0].size as usize, &moption)
-        .expect("Failed to MMAP");
-    println!("Addr start at {} with size of {}", map.data() as u32, available_modes[0].size);
-    // unsafe { libc::memset(map.data() as *mut libc::c_void, 0, available_modes[0].size as usize); };
+    // Creating framebuffer
+    let ref active_mode = available_modes[0];
+    let mut buf = buffer::DrmBuffer::new(file.as_raw_fd(), active_mode.width, active_mode.height);
 
     // Set CRTC
     drm::drm_mode::set_crtc(
         file.as_raw_fd(),
         available_modes[0].crtc.get_crtc_id(),
-        buffer_id,
+        buf.id,
         0,
         0,
         &[available_modes[0].conn.get_connector_id()],
@@ -156,16 +96,18 @@ fn main() {
     ).expect("Failed SET_CRTC");
 
     // Draw
-    for i in 0..50 {
-        for j in 0..available_modes[0].height {
-            for k in 0..available_modes[0].width {
-                let r: u32 = unsafe { libc::rand() as u32 % 0xff + ((libc::rand() as u32).wrapping_mul(10)) };
-                let g: u32 = unsafe { libc::rand() as u32 % 0xff + ((libc::rand() as u32).wrapping_mul(10)) };
-                let b: u32 = unsafe { libc::rand() as u32 % 0xff + ((libc::rand() as u32).wrapping_mul(10)) };
-                // println!("R {} : G {} : B {}", r, g, b);
+    for _ in 0..50 {
+        for j in 0..buf.height {
+            for k in 0..buf.width {
+                let r: u8 = unsafe { (libc::rand() as u8 % 0xff).wrapping_add((libc::rand() as u8).wrapping_mul(10)) };
+                let g: u8 = unsafe { (libc::rand() as u8 % 0xff).wrapping_add((libc::rand() as u8).wrapping_mul(10)) };
+                let b: u8 = unsafe { (libc::rand() as u8 % 0xff).wrapping_add((libc::rand() as u8).wrapping_mul(10)) };
 
-                let offset = available_modes[0].stride * j as u32 + (k as u32 * 4 as u32);
-                unsafe { std::ptr::write(map.data().offset(offset as isize) as *mut u32, (r << 16) | (g << 8) | b); }
+                let color: u32 = ((r as u32) << 16) | ((g as u32) << 8) | b as u32;
+                // println!("R {} + G {} + B {} = Color {}", r, g, b, color);
+
+                let offset = buf.stride * j as u32 + (k as u32 * 4 as u32);
+                buf.write(offset, color);
             }
         }
     }
